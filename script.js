72c3712ce282f71fa1f0f721c0e261707ef117c6
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 10:26:42 +0000

    Make a custom filter
    Change values of gender and with connection with this
    custom filter these values changed to good text

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 var myApp = angular
 				.module("myModule", [])
+				.filter("gender", function() {
+					return function(gender) {
+						switch(gender) {
+							case 1:
+								return "Male";
+							case 2:
+								return "Female";
+							case 3:
+								return "Not disclosed";
+							
+						}
+					}
+				})
 				.controller("myController",  function($scope) {
 					var employees = [
-						{name: "Ben", salary: 55555,gender: "Male", city: "London"},
-						{name: "cBen",  salary: 45555,gender: "Female", city: "Warsaw"},
-						{name: "cBen", salary: 15555,gender: "Male", city: "Poznan"},
-						{name: "aBen",  salary: 25555,gender: "Male", city: "Gdansk"},
-						{name: "bBen",  salary: 35555,gender: "Male", city: "Bratyslawa"}
+						{name: "Ben", salary: 55555,gender: 1, city: "London"},
+						{name: "cBen",  salary: 45555,gender: 1, city: "Warsaw"},
+						{name: "cBen", salary: 15555,gender: 2, city: "Poznan"},
+						{name: "aBen",  salary: 25555,gender: 3, city: "Gdansk"},
+						{name: "bBen",  salary: 35555,gender: 1, city: "Bratyslawa"}
 					];
 					
 					$scope.employees=employees;
