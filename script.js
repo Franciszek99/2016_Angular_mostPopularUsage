467f5c2f70f2afe2bfa40ca5454280eaa1c0baac
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 09:02:19 +0000

    Done a filter searching by name and city in the same time
    Function search checks if any words in table contain wrote
    text. If yes return true if no return false. !=-1 means
    that function found something. Value "-1" is a default
    when function didnt find text

--- script.js
+++ script.js
@@ -11,5 +11,19 @@ var myApp = angular
 					
 					$scope.employees=employees;
 					
+					
+					$scope.search = function(item){
+						if($scope.searchText == undefined){
+							return true;
+						}
+						else{
+							if (item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) !=-1 || item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) !=-1)
+							{
+								return true;
+							}
+						}
+						return false;
+					}
+					
 				});
 					
