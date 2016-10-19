116ec588db3ce2de710a8153d8d66d1d3f1c466c
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 05:13:23 +0000

    Add three functions in script

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
 	var myApp = angular
 					.module("myModule", [])
 					.controller("myController",  function($scope) {
-						var countries = [
-							{Name: "UK", 
-							cities: [ 
-								{name: "London"}, 
-								{name: "Menchaster"}, 
-								{name: "London2"}
-								]
-								
-							}, 
-							{Name: "UK1", 
-							cities: [ 
-								{name: "London"}, 
-								{name: "Menchaster"}, 
-								{name: "London2"}
-								]
-								
-							},
-							{Name: "UK2", 
-							cities: [ 
-								{name: "London"}, 
-								{name: "Menchaster"}, 
-								{name: "London2"}
-								]
-								
-							} 
-						];
-						$scope.countries = countries;
+						var technologies = [
+							{Name: "C#", likes:0, dislikes:0},
+							{Name: "ASP.NET", likes:0, dislikes:0},
+							{Name: "SQL", likes:0, dislikes:0},
+							{Name: "AngularJS", likes:0, dislikes:0}
+							
+							];						
+							$scope.technologies = technologies;
+							$scope.incrementLikes = function(technology) {
+								technology.likes++;
+							}
+							
+							$scope.incrementDislikes = function(technology) {
+								technology.likes++;
+							}
 	});
 	
