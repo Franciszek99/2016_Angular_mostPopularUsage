213f43ad055fc97387bcacb54c2b1b8823cee310
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 08:25:16 +0000

    prepare code for search filter part_2

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
-	var myApp = angular
-					.module("myModule", [])
-					.controller("myController",  function($scope) {
-						var employees = [
-							{name: "Ben", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "cBen", dateOfBirth: new Date("november 21, 1980"),gender: "Female", salary: 55000},
-							{name: "cBen", dateOfBirth: new Date("november 22, 1980"),gender: "Male", salary: 55000},
-							{name: "aBen", dateOfBirth: new Date("november 25, 1980"),gender: "Male", salary: 55000},
-							{name: "bBen", dateOfBirth: new Date("november 28, 1980"),gender: "Male", salary: 55000}
-						];
-						
-						$scope.employees=employees;
-						$scope.sortColumn="dateOfBirth";
-						$scope.reverseSort = false;
-						
-						$scope.sortData = function(column) {
-							$scope.reverseSort = 
-							($scope.sortColumn == column) ? !$scope.reverseSort : false;
-							$scope.sortColumn = column;
-						}
-						
-						$scope.getSortClass = function(column){
-							if ($scope.sortColumn == column) {
-								return $scope.reverseSort ? 'arrow-down' : 'arrow-up'
-							}
-									return '';
-						}
-					});
-						
-	
+var myApp = angular
+				.module("myModule", [])
+				.controller("myController",  function($scope) {
+					var employees = [
+						{name: "Ben", salary: 55555,gender: "Male", city: "London"},
+						{name: "cBen",  salary: 45555,gender: "Female", city: "Warsaw"},
+						{name: "cBen", salary: 15555,gender: "Male", city: "Poznan"},
+						{name: "aBen",  salary: 25555,gender: "Male", city: "Gdansk"},
+						{name: "bBen",  salary: 35555,gender: "Male", city: "Bratyslawa"}
+					];
+					
+					$scope.employees=employees;
+					
+				});
+					
