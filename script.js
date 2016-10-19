e38343dcb2ce4177b5377d44335fe868e03e5dc8
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 03:51:22 +0000

    Simple example to show how view changes a model and model changes a view

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 	var myApp = angular
 					.module("myModule", [])
 					.controller("myController",  function($scope) {
-						var country = {
-							name : "Polska",
-							capital: "Warszawa",
-							flag: "2000px-Flag_of_Poland_(normative).svg.png"
-						};
-			$scope.country = country;						
+						$scope.message= "hello angular"				
 	});
 	
