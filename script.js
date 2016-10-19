37631bb6858b90e7ba63f30fc27e9d74bea2965c
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 07:05:27 +0000

    Prepare for next usege && orderBy: false(default) or true
    delete code line, that I wouldnt use and add order in names

--- script.js
+++ script.js
@@ -3,13 +3,13 @@
 					.controller("myController",  function($scope) {
 						var employees = [
 							{name: "Ben", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "Ben", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "Ben", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "Ben", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "Ben", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000}
+							{name: "aBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
+							{name: "cBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
+							{name: "aBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
+							{name: "bBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000}
 						];
 						
 						$scope.employees=employees;
-						$scope.rowLimit = 3;
+
 	});
 	
