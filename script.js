5ac430b921414a42de9a6f55468a1224d878d9ec
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 04:28:51 +0000

    Add a table in controller and show it on window

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 	var myApp = angular
 					.module("myModule", [])
 					.controller("myController",  function($scope) {
-						var employee = {
-							firstName: "Ben",
-							lastName: "Hasting",
-							gender: "Male"
-						};
-						
-						$scope.employee=employee;
+						var employees = [
+							{firstName: "Ben", lastName: "Hastings", gender:"Male", salary: 55000},
+							{firstName: "Ben1", lastName: "Hastings", gender:"Male", salary: 55000},
+							{firstName: "Ben2", lastName: "Hastings", gender:"Male", salary: 55000},
+							{firstName: "Ben3", lastName: "Hastings", gender:"Male", salary: 55000},
+							{firstName: "Ben4", lastName: "Hastings", gender:"Male", salary: 55000}
+						];
+						$scope.employees = employees;
 	});
 	
