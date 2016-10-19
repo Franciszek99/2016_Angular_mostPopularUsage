e16354d38f988ea24e5fb99465ae0ed8727e320e
Franciszek Milczarski <franko.gen@gmail.com>
2016-10-19 07:15:02 +0000

    Complete a possibility to sort elements in columns
    Add a selector and four option to chose. ASC is like + and DESC like -. Moreover its work great.

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 					.controller("myController",  function($scope) {
 						var employees = [
 							{name: "Ben", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "aBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "cBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "aBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000},
-							{name: "bBen", dateOfBirth: new Date("november 23, 1980"),gender: "Male", salary: 55000}
+							{name: "cBen", dateOfBirth: new Date("november 21, 1980"),gender: "Female", salary: 55000},
+							{name: "cBen", dateOfBirth: new Date("november 22, 1980"),gender: "Male", salary: 55000},
+							{name: "aBen", dateOfBirth: new Date("november 25, 1980"),gender: "Male", salary: 55000},
+							{name: "bBen", dateOfBirth: new Date("november 28, 1980"),gender: "Male", salary: 55000}
 						];
 						
 						$scope.employees=employees;
+						$scope.sortColumn="name";
 
 	});
 	
